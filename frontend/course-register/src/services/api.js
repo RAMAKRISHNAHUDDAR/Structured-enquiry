f67caf6675816580cc78c9d7daa796bd64cc2166
Ramakrishna Huddar <ramakrishnahuddar@gmail.com>
2026-03-15 14:54:39 +0530

    fix production API url

--- frontend/course-register/src/services/api.js
+++ frontend/course-register/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://course-registration-qoao.onrender.com/api",
 });
 
 // Add Authorization header to all requests if token exists
